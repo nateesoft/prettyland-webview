5ddb6bec03a2ac4f8f1f1876093c124d54403e47
natee.live@gmail.com <natee.live@gmail.com>
2021-08-08 11:57:38 +0700

    init react-native init

---
+++ myWebview/src/notification.android.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Push Notification</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
