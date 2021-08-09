337deddb72ba47959840fddc7eac407a50588258
natee.live@gmail.com <natee.live@gmail.com>
2021-08-10 00:01:09 +0700

    complete read firebase realtime

--- prettyLandFirebase/src/screen/HomeScreen.js
+++ prettyLandFirebase/src/screen/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button, StyleSheet, LogBox } from 'react-native';
+import firebase from '../../firebaseSetup';
+
+LogBox.ignoreLogs(['Setting a timer for a long period of time']);
 
 export default function HomeScreen({ navigation }) {
   const goDetail = () => {
     navigation.navigate('Detail');
   };
+
+  useEffect(() => {
+    console.log('Initial Home Screen');
+    firebase
+      .database()
+      .ref('demo/bank_account')
+      .once('value', snapshot => {
+        console.log(snapshot.val());
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
